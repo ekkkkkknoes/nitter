56d9b8b24111339396a178d6bea4542eb2a0ce8a
Ekkie <soelam@live.de>
2023-02-14 20:39:54 +0100

    Cleanup

--- public/js/copyUrlBtn.js
+++ public/js/copyUrlBtn.js
@@ -1,5 +1,4 @@
 window.addEventListener("load", function() {
-    if (old) old();
     var btns = document.querySelectorAll(".icon-bird");
     Array.from(btns).forEach(btn => {
         if (btn) btn.onclick = function(event) {
