31e2718cb5b843a2a91881c788fc56922fd9b167
Sönke Lambert <soelam@live.de>
2023-02-20 22:31:27 +0100

    Use copy-url class instead of icon-bird class

--- public/js/copyUrlBtn.js
+++ public/js/copyUrlBtn.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function() {
-    var btns = document.querySelectorAll(".icon-bird");
+    var btns = document.querySelectorAll(".copy-url");
     Array.from(btns).forEach(btn => {
         if (btn) btn.onclick = function(event) {
             event.preventDefault();
